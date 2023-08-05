c759417070d1f5a1b3e6ddfa35f01cebb940e9df
Ruben Mantilla <-h>
2023-08-05 03:45:47 -0500

    refactor!: proyects in json

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 // toggle icon navbar
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
+// scroll sections
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+// enviar correo
+const btn = document.getElementById("button");
+const form = document.getElementById("form");
+// Show more projects
+let showMore = document.getElementById("btn-show-projects");
+let sectionProjects = document.getElementById("projects");
+let hide = true;
+// fetch proyects
+const proyects = document.getElementById("proyects-container");
+// Current year
+const currentYear = document.getElementById("current-year");
+
+// Current year
+currentYear.innerText = new Date().getFullYear();
+
+// fetch proyects
+fetch("json/proyects.json")
+  .then((res) => res.json())
+  .then((data) => {
+    fetchProyects(data);
+  })
+  .catch((error) => console.error("Error al cargar el archivo JSON:", error));
 
 menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
@@ -8,15 +33,12 @@ menuIcon.onclick = () => {
 };
 
 // scroll sections
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
-
 window.onscroll = () => {
-  sections.forEach((sec) => {
+  sections.forEach((section) => {
     let top = window.scrollY;
-    let offset = sec.offsetTop - 100;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
+    let offset = section.offsetTop - 100;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
     if (top >= offset && top < offset + height) {
       // active navbar links
       navLinks.forEach((links) => {
@@ -38,9 +60,6 @@ window.onscroll = () => {
 };
 
 // enviar correo
-const btn = document.getElementById("button");
-const form = document.getElementById("form");
-
 window.onload = function () {
   emailjs.init("wnBpZLdftA09_3-7A");
 
@@ -70,14 +89,51 @@ window.onload = function () {
   });
 };
 
-let showMore = document.getElementById("btn-show-projects");
-let sectionProjects = document.getElementById("projects");
-let hide = true
-
+// Show more projects
 showMore.onclick = () => {
   sectionProjects.classList.toggle("show-projects");
   hide = !hide;
-  showMore.innerHTML = (hide)
+  showMore.innerHTML = hide
     ? `<i class='bx bx-chevrons-down'></i> Ver más`
     : `<i class='bx bx-chevrons-up'></i> Ver menos`;
 };
+
+function fetchProyects(data) {
+  data.cards.forEach((proyect) => {
+    proyects.innerHTML += `
+      <article class="proyect__item">
+        <a
+          title="${proyect.title}"
+          href="${proyect.url}"
+          target="_blank">
+          <img
+            title="${proyect.title}"
+            loading="lazy"
+            src="${proyect.image}"
+            alt="${proyect.alt_image}}"
+          />
+        </a>
+
+        <div class="proyect__item__content">
+          <h3>${proyect.title}</h3>
+          <p>${proyect.description}</p>
+          <div class="proyect__item__content__actions">
+            <a
+              title="Ver proyecto"
+              href="${proyect.url}"
+              target="_blank"
+              class="btn"
+              >Ver proyecto</a
+            >
+            <a
+              title="Ver código fuente"
+              href="${proyect.repository}"
+              target="_blank"
+              >Repositorio</a
+            >
+          </div>
+        </div>
+      </article>
+    `;
+  });
+}
